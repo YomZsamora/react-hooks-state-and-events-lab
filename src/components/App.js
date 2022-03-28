fa3a66d31f5b1cb41a063e08ab4d311f2328ce3c
yomZsamora <samora.y@adzumi.co.ke>
2022-03-28 20:47:52 +0300

    Changing className on adding an item to cart. Using React Hook.

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,24 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-   const [currentMode, setTheme] = useState();
+	const [currentMode, setTheme] = useState();
 
-   function changeCurrentMode() {
-      setTheme(!currentMode)
-   }
+	function changeCurrentMode() {
+		setTheme(!currentMode)
+	}
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
   const appClass = currentMode ? "App dark" : "App light";
 
   return (
-    <div className={appClass}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={changeCurrentMode} >Dark Mode</button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
+	 <div className={appClass}>
+		<header>
+		  <h2>Shopster</h2>
+		  <button onClick={changeCurrentMode} >Dark Mode</button>
+		</header>
+		<ShoppingList items={itemData} />
+	 </div>
   );
 }
 
