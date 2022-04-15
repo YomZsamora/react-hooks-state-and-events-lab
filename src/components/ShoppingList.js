c2b65e0c03a4853cbfac53d206c82b39df2fb0c1
Yommie Samora <yomz@Yommies-MacBook-Pro.local>
2022-04-15 12:03:36 +0300

    Completed Lab.

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,31 +3,31 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
 
-   const [selectedCategory, filterCategory] = useState(items);
+	 const [selectedCategory, filterCategory] = useState(items);
 
-   function getSelectedCategory(event) {
-      const category = event.target.value
-      const categoryItems = items.filter( item => item.category === category )
-      filterCategory(categoryItems)
-   }
+	 function getSelectedCategory(event) {
+			const category = event.target.value
+			const categoryItems = items.filter( item => item.category === category )
+			filterCategory(categoryItems)
+	 }
 
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={getSelectedCategory} >
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
-      <ul className="Items">
-        {selectedCategory.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className="ShoppingList">
+			<div className="Filter">
+				<select name="filter" onChange={getSelectedCategory} >
+					<option value="All">Filter by category</option>
+					<option value="Produce">Produce</option>
+					<option value="Dairy">Dairy</option>
+					<option value="Dessert">Dessert</option>
+				</select>
+			</div>
+			<ul className="Items">
+				{selectedCategory.map((item) => (
+					<Item key={item.id} name={item.name} category={item.category} />
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default ShoppingList;
